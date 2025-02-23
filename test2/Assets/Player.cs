3206e84360f0bf8085d20102115f1764973600c8
Leegaeun1 <igaeun@gmail.com>
2025-02-23 16:55:10 +0900

    202240223
    문 회전과 이동 수정

--- test2/Assets/Player.cs
+++ test2/Assets/Player.cs
@@ -1,63 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     public float Speed = 10.0f;     // 움직이는 속도. public으로 설정하여 유니티 화면에서 조정할 수 있다.
-
-    float h, v;                     // 가로축과 세로축을 담을 변수를 전역변수로 생성. FixedUpdate에서 직접 생성하지 않은 이유는
-                                    // 이후 다른 함수에서도 접근할 것이기 때문.
+    float ry;
+    float h, v;
+    public float rotateSpeed=100.0f;
     bool isWalking = false;
     Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        ry=transform.eulerAngles.y;
         anim=GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         
+
     }
 
-    private void OnCollisionStay(Collision collision)
-    {
-        //if (collision.gameObject.CompareTag("Doors"))
-        //{
-        //    if (Input.GetKeyDown(KeyCode.E))
-        //    {
 
-        //        // 문을 90도 회전
-        //        collision.gameObject.transform.rotation = Quaternion.Euler(
-        //            collision.transform.rotation.eulerAngles.x,
-        //            collision.transform.rotation.eulerAngles.y + 90f,
-        //            collision.transform.rotation.eulerAngles.z
-        //        );
-        //    }
-        //}
-    }
-
-    // 이동 관련 함수를 짤 때는 Update보다 FixedUpdate가 더 효율이 좋다고 한다. 그래서 사용했다.
     void FixedUpdate()
     {
-        // Point 1.
-        h = Input.GetAxis("Horizontal");        // 가로축
-        v = Input.GetAxis("Vertical");          // 세로축
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(0, 0, v) * Speed * Time.deltaTime);
+        ry += h * rotateSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(0, ry, 0));
 
+        colliderObject();
+        Walking();
+
+        // Point 2.
+        //transform.position += new Vector3(-h, 0, -v) * Speed * Time.deltaTime;
+    }
+    void colliderObject()
+    {
+        Vector3 startPosition = transform.position + Vector3.up * 5f;
+        Debug.DrawRay(startPosition, transform.forward * 10, Color.red);
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(startPosition, transform.forward, out rayHit, 10, LayerMask.GetMask("Walls")))
+        {
+            print("충돌!");
+            v = 0;
+        }
+    }
+    void Walking()
+    {
         if (h != 0 || v != 0)
         {
-            isWalking= true; 
+            isWalking = true;
         }
         else
         {
             isWalking = false;
         }
-
         anim.SetBool("isWalk", isWalking);
-        // Point 2.
-        transform.position += new Vector3(h, 0, v) * Speed * Time.deltaTime;
     }
 }
